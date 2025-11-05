5ed81cdb13a6915d3412ee9d9c4336b8db677406
Sacha Macé <sacha.mace@gmail.com>
2025-11-05 09:21:39 +0100

    :construction: formulaire creer compte html fait !

--- back_office/creercompte.php
+++ back_office/creercompte.php
@@ -9,6 +9,7 @@
 
 </head>
 
+<<<<<<< HEAD
 <body class="body__creercompte">
     <!-- /**<aside><img src="front_end/assets/images/logo_Alizon.png" alt="Logo Alizon" class="logo" width="270" height ="115"></aside>**/ -->
     <form class="form__creercompte" method="post" enctype = "multipart/form-data">
@@ -32,6 +33,28 @@
         <input type="confirm" id="confirm" name="confirm" placeholder="Confirmer le mot de passe" required />
         <!-- Bouton de création de compte -->
         <br />
+=======
+<body>
+    <img href="front_end/assets/images/logo_Alizon.png">
+    <form method="post" enctype = "multipart/form-data">
+        <h2>Créer un compte</h2>
+        <!-- Numéro de Siren -->
+        <label for="num_siren">Numéro de SIREN</label>
+        <input type="text" id="num_siren" name="num_siren" required />
+        <br />
+        <!-- Raison Sociale -->
+        <label for="raison_sociale">Raison Sociale</label>
+        <input type="text" id="raison_sociale" name="raison_sociale" required />
+        <br />
+        <!-- Numéro de Téléphone -->
+        <label for="tel">Numéro de Télephone</label>
+        <input type="tel" id="tel" name="tel" />
+        <br />
+        <!-- Email -->
+        <label for="email">Adresse Mail</label>
+        <input type="email" id="email" name="email" required />
+        <!-- Bouton de création de compte -->
+>>>>>>> f51834d (:construction: formulaire creer compte html fait !)
         <input type="submit" value="Soumettre" />
     </form>
 </body>

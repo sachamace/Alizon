061cac5919efb5c015db7fc0c1494c5f7c9cacb3
Sacha Macé <sacha.mace@gmail.com>
2025-11-05 17:05:28 +0100

    :construction: Tout les test sont fait en php

--- back_office/creercompte.php
+++ back_office/creercompte.php
@@ -1,3 +1,36 @@
+<?php
+    $erreur_siren ="";
+    $erreur_tel ="";
+    $erreur_mail ="";
+    $erreur_mdp ="";
+    $num_siren = "" ;
+    $tel = "" ;
+    $mail= "" ;
+    $mdp = "" ;
+    $mdpconfirm = "";
+    if($_SERVER["REQUEST_METHOD"] === "POST"){
+        $num_siren = trim($_POST['num_siren']);
+        $tel = trim($_POST['tel']);
+        $mail = trim($_POST['email']);
+        $mdp = trim($_POST['motdepasse']);
+        $mdpconfirm = trim($_POST['confirm']);
+        if (!preg_match("/^[0-9]{9}$/", $num_siren)) {
+            $erreur_siren = "Total de chiffre invalides ! Nombre de chiffre qu'on requière = 9";
+        }
+        if(!preg_match("/^[0-9]{10}$/",$tel)){
+            $erreur_tel = "Total de chiffres invalides ! Nombre de chiffre qu'on requière = 10";
+        }
+        if(!preg_match("/^.{12,}$/",$mdp)){
+            $erreur_mdp="Nombre de caractère trop petit ! Nombre de caractère minimale = 12";
+        }
+        if(strcmp($mdp,$mdpconfirm) != 0){
+            $erreur_confirm = "La confirmation du mot de passe ne correspond pas au mot de passe que vous avez mis.";
+        }
+    }
+    
+?>
+
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -9,29 +42,59 @@
 
 </head>
 
-<body>
-    <img href="front_end/assets/images/logo_Alizon.png">
-    <form method="post" enctype = "multipart/form-data">
+<body class="body__creercompte">
+    <form class="form__creercompte" method="post" enctype = "multipart/form-data">
         <h2>Créer un compte</h2>
         <!-- Numéro de Siren -->
-        <input type="text" id="num_siren" name="num_siren" placeholder="Numéro de SIREN"required />
+        <input class="input__creercompte" type="text" id="num_siren" name="num_siren" placeholder="Numéro de SIREN *" value ="<?= $num_siren?>"required />
+        <?php
+            if (!empty($erreur_siren)){
+                echo "<span class='error-message'>$erreur_siren</span>";
+            }
+        ?>
         <br />
         <!-- Raison Sociale -->
-        <input type="text" id="raison_sociale" name="raison_sociale" placeholder="Raison Sociale"required />
+        <select class="select__raison" id="raison" name="raison">
+                <option disabled selected>Raison Sociale</option>
+                <option value="SA">SA</option>
+                <option value="SAS">SAS</option>
+                <option value="SARL">SARL</option>
+                <option value="EURL">EURL</option>
+                <option value="SASU">SASU</option>
+                <option value="SCP">SCP</option>
+        </select>
         <br />
         <!-- Numéro de Téléphone -->
-        <input type="tel" id="tel" name="tel" placeholder="Numéro de Téléphone" required />
+        <input class="input__creercompte" type="tel" id="tel" name="tel" placeholder="Numéro de Téléphone *" value ="<?= $tel?>"required />
         <br />
+        <?php
+            if (!empty($erreur_tel)){
+                echo "<span>$erreur_tel</span>";
+            }
+        ?>
         <!-- Email -->
-        <input type="email" id="email" name="email" placeholder="Adresse Mail"required />
+        <input class="input__creercompte" type="email"  name="email" placeholder="Adresse Mail *" value ="<?= $mail?>"required />
         <br />
         <!-- Mot de passe -->
-        <input type="mdp" id="mdp" name="motdepasse" placeholder="Mot de passe"required />
+        <input class="input__creercompte" type="password"  name="motdepasse" placeholder="Mot de passe *" value ="<?= $mdp?>"required />
         <br />
+        <?php
+            if (!empty($erreur_mdp)){
+                echo "<span>$erreur_mdp</span>";
+            }
+        ?>
         <!-- Confirmer le mot de passe -->
-        <input type="confirm" id="confirm" name="confirm" placeholder="Confirmer le mot de passe" required />
+        <input class="input__creercompte"  type="password" name="confirm" placeholder="Confirmer le mot de passe *" required />
         <!-- Bouton de création de compte -->
-        <input type="submit" value="Soumettre" />
+        <br />
+        <?php
+            if(!empty($erreur_confirm)){
+                echo "<span>$erreur_confirm</span>";
+            }
+        ?>
+        <input class="input__creercompte--submit" type="submit" value="Créer un compte" />
+
+        <label><a href="connecter.php">Se connecter</a></label>
     </form>
 </body>
 </html>

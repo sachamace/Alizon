e75414af12b0bbbb2993a05e4f3a3f7b7afc28bc
Sacha Macé <sacha.mace@gmail.com>
2025-11-05 13:25:51 +0100

    :construction: Création d'un compte CSS 50%

--- back_office/creercompte.php
+++ back_office/creercompte.php
@@ -9,26 +9,29 @@
 
 </head>
 
-<body>
-    <img href="front_end/assets/images/logo_Alizon.png">
-    <form method="post" enctype = "multipart/form-data">
+<body class="body__creercompte">
+    <!-- /**<aside><img src="front_end/assets/images/logo_Alizon.png" alt="Logo Alizon" class="logo" width="270" height ="115"></aside>**/ -->
+    <form class="form__creercompte" method="post" enctype = "multipart/form-data">
         <h2>Créer un compte</h2>
         <!-- Numéro de Siren -->
-        <label for="num_siren">Numéro de SIREN</label>
-        <input type="text" id="num_siren" name="num_siren" required />
+        <input type="text" id="num_siren" name="num_siren" placeholder="Numéro de SIREN"required />
         <br />
         <!-- Raison Sociale -->
-        <label for="raison_sociale">Raison Sociale</label>
-        <input type="text" id="raison_sociale" name="raison_sociale" required />
+        <input type="text" id="raison_sociale" name="raison_sociale" placeholder="Raison Sociale"required />
         <br />
         <!-- Numéro de Téléphone -->
-        <label for="tel">Numéro de Télephone</label>
-        <input type="tel" id="tel" name="tel" />
+        <input type="tel" id="tel" name="tel" placeholder="Numéro de Téléphone" required />
         <br />
         <!-- Email -->
-        <label for="email">Adresse Mail</label>
-        <input type="email" id="email" name="email" required />
+        <input type="email" id="email" name="email" placeholder="Adresse Mail"required />
+        <br />
+        <!-- Mot de passe -->
+        <input type="mdp" id="mdp" name="motdepasse" placeholder="Mot de passe"required />
+        <br />
+        <!-- Confirmer le mot de passe -->
+        <input type="confirm" id="confirm" name="confirm" placeholder="Confirmer le mot de passe" required />
         <!-- Bouton de création de compte -->
+        <br />
         <input type="submit" value="Soumettre" />
     </form>
 </body>

926d994ab2b03d1fc1c6ded1b27f918eed50be26
Sacha Macé <sacha.mace@gmail.com>
2025-11-10 14:18:42 +0100

    :bug: Session de connexion ne marche pas

--- front_office/front_end/html/seconnecter.php
+++ front_office/front_end/html/seconnecter.php
@@ -1,6 +1,75 @@
 <?php
     session_start();
     include 'config.php';
+    $stmt = $pdo->query("SELECT version();");
+?>
+
+<?php
+    $errreur_mdp = "";
+    $erreur_ident = "";
+    $mdp = "";
+    $email = "" ;
+    if($_SERVER["REQUEST_METHOD"]==="POST"){
+        $email = trim($_POST['adresse_mail']);
+        $mdp = trim($_POST['motdepasse']);
+        $user_sql = $pdo->prepare("SELECT * FROM public.identifiants WHERE login = :login");
+        $user_sql->execute([$email]);
+        $user = $user_sql->fetch();
+
+        if($user){
+            if(password_verify($mdp,$user['mdp'])){
+                $_SESSION['login'] = $user['login'];
+                $_SESSION['mdp'] = $user['mdp'];
+                header("Location: index.php");
+                exit();
+            }
+            else{
+                $errreur_mdp = "Mot de passe Incorrect";
+            }
+        }
+        else{
+            $erreur_ident = "Identifiant Incorrect";
+        }
+
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Se connecter - Clients</title>
+    <link rel="stylesheet" href="../front_end/assets/csss/style.css">
+</head>
+
+<body class="body__creercompte">
+    <form action =""class="form__creercompte"  method="post" enctype = "multipart/form-data">
+        <h2>Se connecter</h2>
+        <input class="input__creercompte" type="text"  name="adresse_mail" placeholder="Adresse de Mail" value ="<?= $email?>"required />
+        <br />
+        <?php
+            if (!empty($erreur_ident)){
+                echo "<span class='error-message'>$erreur_ident</span><br />";
+            }
+        ?>
+        <input class="input__creercompte" type="password"  name="motdepasse" placeholder="Mot de passe"required />
+        <br />
+        <?php
+            if (!empty($erreur_mdp)){
+                echo "<span class='error-message'>$erreur_mdp</span><br />";
+            }
+        ?>
+        <input class="input__creercompte--submit" type="submit" value="Se connecter" />
+        <br />
+        <label><a href="createcompte.php">Créer un compte</a></label>
+    </form>
+</body>
+</html>
+<?php
+    session_start();
+    include 'config.php';
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $stmt = $pdo->query("SELECT version();");
 

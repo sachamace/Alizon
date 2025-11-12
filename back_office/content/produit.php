e883561dd5841526d6e0d3a9fecd06d3d693d0ce
Noah-LeGoff <noahlegoff22970@gmail.com>
2025-11-12 12:10:11 +0100

    réer produit

--- back_office/content/produit.php
+++ back_office/content/produit.php
@@ -19,17 +19,12 @@ if (isset($_GET['id']) && isset($_GET['type'])) {
     $categorie = $stmtCat->fetchColumn();
 
     if ($_GET['type'] == "consulter") {
-
-    if ($_GET['type'] == "consulter") {
-
-        if ($_GET['type'] == "consulter") {
-            include 'produit_consulter.php';
-        } else if ($_GET['type'] == 'modifier') {
-            include 'produit_modifier.php';
-        }
+        include 'produit_consulter.php';
+    } else if ($_GET['type'] == 'modifier') {
+        include 'produit_modifier.php';
     }
 } else if (isset($_GET['type']) && $_GET['type'] === 'creer') {
     $stmtCat = $pdo->query("SELECT libelle FROM categorie");
     $categorie = $stmtCat->fetchAll();
     include 'produit_creer.php';
-} ?>
+}?>

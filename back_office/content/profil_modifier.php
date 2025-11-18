ef215300b8b0a2abc55f15a990cb683cbcd2b2ee
Noah-LeGoff <noahlegoff22970@gmail.com>
2025-11-18 16:55:04 +0100

    plus que le post a faire

--- back_office/content/profil_modifier.php
+++ back_office/content/profil_modifier.php
@@ -3,18 +3,38 @@
         <h2>Votre profil</h2>
         <article>
             <h3>Raison sociale</h3>
-            <input type="text" name="raison_sociale" id="raison_sociale" value="<?php echo htmlentities($profil["raison_sociale"]) ?>">
+            <input type="text" name="raison_sociale" id="raison_sociale"
+                value="<?php echo htmlentities($profil["raison_sociale"]) ?>">
         </article>
 
         <article>
             <h3>Adresse email</h3>
-            <input type="text" name="adresse_mail" id="adresse_mail" value="<?php echo htmlentities($profil["adresse_mail"]) ?>">
+            <input type="text" name="adresse_mail" id="adresse_mail"
+                value="<?php echo htmlentities($profil["adresse_mail"]) ?>">
         </article>
 
-        <article>
+        <article class="password-field" id="toggle-password">
             <h3>Mot de passe</h3>
-            <p>********</p>
+            <div>
+                <p>********</p>
+                <svg xmlns="http://www.w3.org/2000/svg" id="arrow" width="16" height="16" fill="black"
+                    class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+                    <path
+                        d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+                </svg>
+            </div>
         </article>
+
+        <div class="password-edit" id="password-edit">
+            <label>Mot de passe actuel</label>
+            <input type="password" name="mdp_actuel">
+
+            <label>Nouveau mot de passe</label>
+            <input type="password" name="mdp_nouveau">
+
+            <label>Confirmer le nouveau mot de passe</label>
+            <input type="password" name="mdp_confirmation">
+        </div>
 
         <article>
             <h3>Numéro de téléphone</h3>
@@ -32,3 +52,13 @@
         </div>
     </form>
 </section>
+<script>
+    document.getElementById("toggle-password").addEventListener("click", () => {
+        const bloc = document.getElementById("password-edit");
+        const arrow = document.getElementById("arrow");
+
+        bloc.classList.toggle("visible");
+        arrow.classList.toggle("rotate");
+    });
+
+</script>

9c33fba4c806f6677764684d69d78598f6ae4837
Sacha Macé <sacha.mace@gmail.com>
2025-11-20 11:43:48 +0100

    :art: Deux bug régler coté scss

--- front_office/front_end/html/consulterProfilClient.php
+++ front_office/front_end/html/consulterProfilClient.php
@@ -58,25 +58,31 @@ try {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="" crossorigin="anonymous">
 </head>
 <body class="body_profilClient">
-    <header class="disabled">
+    <header class = "disabled">
         <nav>
             <nav>
                 <a href="/index.php"><img src="../assets/images/Logo_TABLETTE.png" height="61" width="110"></a>
                 <a class="notif" href="notification.php"><i class="fa-regular fa-bell icone"></i></a>
                 <form action="recherche.php" method="get" role="search" aria-label="Site search">
                     <label for="site-search"></label>
-                    <input type="search" id="site-search" name="q" placeholder="Recherche un produit, une marque..." />
+                    <input disabled type="search" id="site-search" name="q" placeholder="Recherche un produit, une marque..." />
                     <button type="submit">Search</button>
                 </form>
                 <a href="panier.php" data-panier><i class="fa-solid fa-cart-shopping icone" ></i>Panier</a>
             </nav>
             <nav>
                 <div>
-                    <a href="produitTerroir.php">Produit du Terroir</a>
-                    <a href="modeBretonne.php">Mode Bretonne</a>
-                    <a href="">Artisanat Local</a>
-                    <a href="">Décoration Intérieure</a>
-                    <a href="">Epicerie FIne</a>
+                <?php
+                // On récupère tout le contenu de la table produit
+                $categorie = $pdo->query('SELECT * FROM categorie');
+                // On affiche chaque entrée une à une
+                while ($cat = $categorie->fetch()){ 
+                    $libelle = urlencode($cat['libelle']); 
+                    ?>
+                    <a href="/index.php?categorie=<?php echo $libelle; ?>">
+                        <?php echo $cat['libelle']; ?>
+                    </a>
+                <?php } ?>
                 </div>
                 <?php if($isLogged):?><a href="compte.php"><i class="fa-regular fa-user icone"></i>Mon Compte</a>
                 <?php else: ?><a href="seconnecter.php"></i>S'identifier</a>
@@ -85,7 +91,7 @@ try {
         </nav>
     </header>
     <div class="compte__header">Vos données</div> 
-    <main class="main_profilClient">
+    <main class="main_profilClient" >
         <section class="bloc infoG">
                 <h2>Informations générales</h2>
                 <p>Prénom<span><?= htmlspecialchars($user['prenom']) ?><a href="modifierPrenom.php"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" height="20px" width="20px" class="ModifierDonnée"><!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M505 122.9L517.1 135C526.5 144.4 526.5 159.6 517.1 168.9L488 198.1L441.9 152L471 122.9C480.4 113.5 495.6 113.5 504.9 122.9zM273.8 320.2L408 185.9L454.1 232L319.8 366.2C316.9 369.1 313.3 371.2 309.4 372.3L250.9 389L267.6 330.5C268.7 326.6 270.8 323 273.7 320.1zM437.1 89L239.8 286.2C231.1 294.9 224.8 305.6 221.5 317.3L192.9 417.3C190.5 425.7 192.8 434.7 199 440.9C205.2 447.1 214.2 449.4 222.6 447L322.6 418.4C334.4 415 345.1 408.7 353.7 400.1L551 202.9C579.1 174.8 579.1 129.2 551 101.1L538.9 89C510.8 60.9 465.2 60.9 437.1 89zM152 128C103.4 128 64 167.4 64 216L64 488C64 536.6 103.4 576 152 576L424 576C472.6 576 512 536.6 512 488L512 376C512 362.7 501.3 352 488 352C474.7 352 464 362.7 464 376L464 488C464 510.1 446.1 528 424 528L152 528C129.9 528 112 510.1 112 488L112 216C112 193.9 129.9 176 152 176L264 176C277.3 176 288 165.3 288 152C288 138.7 277.3 128 264 128L152 128z"/></svg></a></span></p>

9b30451cfc31331aade6bca85ca2b0720cea5ed0
Sacha Macé <sacha.mace@gmail.com>
2026-01-15 09:59:52 +0100

    :bug: bug régler ?

--- front_office/front_end/html/session.php
+++ front_office/front_end/html/session.php
@@ -1,5 +1,7 @@
 <?php
-    session_start();
+    if (session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
     // Vérifie si l'utilisateur est connecté
     if (!isset($_SESSION['id']) || !isset($_SESSION['login'])) {
         header("Location: seconnecter.php");
--- front_office/front_end/html/sessionindex.php
+++ front_office/front_end/html/sessionindex.php
@@ -1,5 +1,7 @@
 <?php
-    session_start();
+    if (session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
     // Vérifier si l'utilisateur est connecté
     $isLogged = isset($_SESSION['login']);
 ?>

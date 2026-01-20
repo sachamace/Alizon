640d16b604520da98dacad28f1950afdb28e04f5
enzhao08 <enzo.annic@orange.fr>
2026-01-20 17:07:15 +0100

    Correction bug chiffre carte paiement

--- front_office/front_end/assets/js/paiement.js
+++ front_office/front_end/assets/js/paiement.js
@@ -1,15 +1,11 @@
 const radioCarte = document.getElementById("radio-carte");
 const formCarte = document.getElementById("form-carte");
 
-const radioPaypal = document.getElementById("radio-paypal");
-const formPaypal = document.getElementById("form-paypal");
-
 const carteInput = document.getElementById('carte');
 
 const btnPayer = document.querySelector(".payer-btn");
 
 let carteVisible = false;
-let paypalVisible = false;
 
 btnPayer.disabled = true; //on désactive le bouton
 
@@ -21,36 +17,13 @@ radioCarte.addEventListener("click", () => {
     } else {
         formCarte.classList.remove("hidden");
         carteVisible = true;
-        radioPaypal.checked = false;
-        formPaypal.classList.add("hidden");
-        paypalVisible = false;
     }
 
-    //Si un des 2 forms sont visibles
-    if (carteVisible || paypalVisible) {
+    //Si le formulaire de carte est visible
+    if (carteVisible) {
         btnPayer.removeAttribute('disabled');  //On enleve l'attribut disabled et on active le bouton payer
     } else {
         btnPayer.disabled = true;   //Sinon on laisse l'attribut
-    }
-});
-
-radioPaypal.addEventListener("click", () => {
-    if (paypalVisible) {
-        radioPaypal.checked = false;
-        formPaypal.classList.add("hidden");
-        paypalVisible = false;
-    } else {
-        formPaypal.classList.remove("hidden");
-        paypalVisible = true;
-        radioCarte.checked = false;
-        formCarte.classList.add("hidden");
-        carteVisible = false;
-    }
-
-    if (carteVisible || paypalVisible) {
-        btnPayer.removeAttribute('disabled');
-    } else {
-        btnPayer.disabled = true;
     }
 });
 

3ff7af00fc6dd19d6bd3becd380e8593c75a60f8
Sacha Macé <sacha.mace@gmail.com>
2026-03-02 15:08:06 +0100

    installation de la librairie otphp

---
+++ front_office/front_end/html/desactiverA2f.php
@@ -0,0 +1,11 @@
+<?php
+    include 'config.php';
+    include 'session.php';
+    include 'sessionindex.php';
+    $id_client_connecte = $_SESSION['id_client'];
+    $stmt_a2f = $pdo->prepare("UPDATE compte_client SET a2f = ' ' WHERE id_client = :id_client");
+    $stmt_a2f->execute(['id_client' => $id_client_connecte]);
+
+    header("Location: consulterProfilClient.php");
+    exit();
+?> 

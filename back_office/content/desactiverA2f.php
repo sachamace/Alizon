6371d897b80062197c1cc6929b4ffcbd58f2370b
Sacha Macé <sacha.mace@gmail.com>
2026-03-04 10:05:28 +0100

    :bug: test !

--- back_office/content/desactiverA2f.php
+++ back_office/content/desactiverA2f.php
@@ -4,6 +4,6 @@
     $stmt_a2f = $pdo->prepare("UPDATE compte_vendeur SET codea2f = '' WHERE id_vendeur = :id_vendeur");
     $stmt_a2f->execute(['id_vendeur' => $id_vendeur_connecte]);
 
-    header("Location: index.php?page=profil&type=consulter");
+    header("Location: index.php?page=profil");
     exit();
 ?> 

bfff99eca2243a2406c3d1a2269fdb50d388d0fc
Sacha Macé <sacha.mace@gmail.com>
2026-03-16 08:41:28 +0100

    :bug: test ça

--- front_office/front_end/html/seconnecter.php
+++ front_office/front_end/html/seconnecter.php
@@ -1,13 +1,13 @@
 <?php
-    use OTPHP\TOTP;
+    
     date_default_timezone_set('Europe/Paris');
-    session_start();
+    
     include 'config.php';
     
     require_once '../../../vendor/autoload.php';
-
+    use OTPHP\TOTP;
     
-
+    session_start();
     
 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
